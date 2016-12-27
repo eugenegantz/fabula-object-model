ed99ea12ee41e6914e9bf21889070dd5c2ccc722
Eugene Gantz <eugengantz@gmail.com>
2016-12-27 22:52:44 +0400

    add: возможность определить запрос на выборку номенклатуры на уровне проекта

--- fabula-object-model/data-models/GandsDataModel.js
+++ fabula-object-model/data-models/GandsDataModel.js
@@ -3,19 +3,19 @@
 // ------------------------------------------------------
 // Номенклатура
 
-var DefaultDataModel = require("./DefaultDataModel");
-var IEvents = require("./InterfaceEvents");
-var ObjectA = require("./ObjectA");
-var _utils = require("./../utils");
+var DefaultDataModel = require("./DefaultDataModel"),
+	IEvents = require("./InterfaceEvents"),
+	ObjectA = require("./ObjectA"),
+	_utils = require("./../utils");
 
 // Для совместимости
-var getContextDB = function(){
-	var FabulaObjectModel = require("./../_FabulaObjectModel.js");
-	var DBModel = FabulaObjectModel.prototype._getModule("DBModel");
+var getContextDB = function() {
+	var FabulaObjectModel = require("./../_FabulaObjectModel.js"),
+		DBModel = FabulaObjectModel.prototype._getModule("DBModel");
 
-	if (  this._fabulaInstance ){
+	if (this._fabulaInstance)
 		return this._fabulaInstance.getDBInstance();
-	}
+
 	return DBModel.prototype.getInstance();
 };
 
@@ -23,13 +23,12 @@ var getContextDB = function(){
 /**
  * @constructor
  * */
-var GandsDataModel = function(){
+var GandsDataModel = function() {
 	this.init();
 };
 
-GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvents,{
-	"init" : function(){
-
+GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvents, {
+	"init": function() {
 		this.dbModel = null;
 
 		this.data = [];
@@ -43,18 +42,17 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 		this._init_timestamp = null;
 
 		this._indexData = {}; // after init => Object
-
 	},
 
-	"instances" : [],
+	"instances": [],
 
 	/**
 	 * @return {GandsDataModel}
 	 * */
-	"getInstance" : function(){
-		if (this.instances.length){
+	"getInstance": function() {
+		if (this.instances.length)
 			return this.instances[0];
-		}
+
 		return new GandsDataModel();
 	},
 
@@ -63,65 +61,68 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	 * @param {Object} arg
 	 * @param {Function} arg.callback
 	 * */
-	"load" : function(arg){
+	"load": function(arg) {
 		if (typeof arg == "undefined") arg = Object.create(null);
-		var useCache = typeof arg.useCache == "undefined" ? true : Boolean(arg.useCache);
-		var callback = typeof arg.callback == "function" ? arg.callback : function(){};
-		var self = this;
 
-		var db = getContextDB.call(this);
-		var configRow = {"GSID": "ТСFM", "GSName": "Настройки FOM", "GSKindName":"", "GSCOP":""};
-		var configRowProps = [];
+		var self            = this,
+			useCache        = typeof arg.useCache == "undefined" ? true : Boolean(arg.useCache),
+			callback        = typeof arg.callback == "function" ? arg.callback : function() {},
+			db              = getContextDB.call(this),
+			configRow       = { "GSID": "ТСFM", "GSName": "Настройки FOM", "GSKindName": "", "GSCOP": "" },
+			configRowProps  = [];
 
 		/*#if browser,node*/
 		// Номеклатура
 		db.dbquery({
 			"query": "SELECT pid, extID, property, value FROM Property WHERE ExtID IN(SELECT [value] FROM Property WHERE extClass = 'config' AND property = 'fom-config-entry-gsid')",
-			"callback": function(dbres){
+			"callback": function(dbres) {
 
 				// Конфиг по-умолчанию
 				var dbq = [
-					"SELECT * FROM Gands " 		+
-					"WHERE "							+
-					"GSCOP LIKE '87%' "			+
-					"OR GSCOP LIKE '17%' "		+
-					"OR GSCOP LIKE '27%' "		+
-					"OR GSCOP LIKE '07%' "		+
+					"SELECT * FROM Gands " +
+					"WHERE " +
+					"GSCOP LIKE '87%' " +
+					"OR GSCOP LIKE '17%' " +
+					"OR GSCOP LIKE '27%' " +
+					"OR GSCOP LIKE '07%' " +
 					"OR GSID LIKE 'ТСПо%' "
 				];
 
-				if (dbres.recs.length){
-					for(var c=0; c<dbres.recs.length; c++){
+				if (dbres.recs.length) {
+					for (var c = 0; c < dbres.recs.length; c++) {
 						// Запрос из конфига
-						if (dbres.recs[c].property == "запрос-номенклатура"){
+						if (dbres.recs[c].property == "запрос-номенклатура")
 							dbq[0] = "SELECT * FROM Gands WHERE " + dbres.recs[c].value;
-						}
+
 						configRowProps.push(dbres.recs[c]);
 					}
 				}
 
+				// На случай если необходимо переопределить запрос на уровне проекта
+				if (self.sql) dbq[0] = self.sql;
+
 				// Расширение номенклатуры
 				dbq.push(
-					"SELECT * FROM GandsExt "				+
-					"WHERE "										+
-					"GSExID IN ("									+
-					dbq[0].replace(/[*]/gi, "GSID")			+
+					"SELECT * FROM GandsExt " +
+					"WHERE " +
+					"GSExID IN (" +
+					dbq[0].replace(/[*]/gi, "GSID") +
 					")"
 				);
 
 				// Свойства номенклатуры
 				dbq.push(
-					"SELECT pid, extID, property, value FROM Property "	+
-					"WHERE "																+
-					"ExtID IN ("															+
-					dbq[0].replace(/[*]/gi, "GSID")									+
+					"SELECT pid, extID, property, value FROM Property " +
+					"WHERE " +
+					"ExtID IN (" +
+					dbq[0].replace(/[*]/gi, "GSID") +
 					")"
 				);
 
-				if (db){
+				if (db) {
 					db.dbquery({
-						"query" : dbq.join("; "),
-						"callback" : function(res){
+						"query": dbq.join("; "),
+						"callback": function(res) {
 							self._afterLoad(
 								{
 									"data": res[0].recs.concat(configRow),
@@ -141,7 +142,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 		/*#end*/
 
 		/*  #if browser-s */
-		if (  _utils.isBrowser()  ){
+		if (_utils.isBrowser()) {
 			var Ajax = require("./../browser/Ajax");
 			Ajax.req({
 				"url": self._fabulaInstance.url,
@@ -152,8 +153,8 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 						"useCache": useCache
 					}
 				},
-				"callback": function(err, http){
-					if (err){
+				"callback": function(err, http) {
+					if (err) {
 						callback(err);
 						return;
 					}
@@ -168,14 +169,14 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	/**
 	 * @ignore
 	 * */
-	"_afterLoad": function(dbres, callback){
+	"_afterLoad": function(dbres, callback) {
 		var self = this;
 		self.data = dbres.data;
 		self.state = 1;
 
 		var c, L, gandsRef = Object.create(null);
 
-		for(c=0; c<self.data.length; c++){
+		for (c = 0; c < self.data.length; c++) {
 			gandsRef[self.data[c].GSID] = self.data[c];
 			if (!self.data[c].gandsExtRef) self.data[c].gandsExtRef = [];
 			if (!self.data[c].gandsPropertiesRef) self.data[c].gandsPropertiesRef = [];
@@ -183,19 +184,19 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 		var gandsExt = dbres.ext;
 
-		for(c=0; c<gandsExt.length; c++){
-			if (  typeof gandsRef[gandsExt[c].GSExID] == "undefined"  ) continue;
+		for (c = 0; c < gandsExt.length; c++) {
+			if (typeof gandsRef[gandsExt[c].GSExID] == "undefined") continue;
 			gandsRef[gandsExt[c].GSExID].gandsExtRef.push(gandsExt[c]);
 		}
 
 		var gandsProps = dbres.props;
 
-		for(c=0; c<gandsProps.length; c++){
-			if (  typeof gandsRef[gandsProps[c].extID] == "undefined"  ) continue;
+		for (c = 0; c < gandsProps.length; c++) {
+			if (typeof gandsRef[gandsProps[c].extID] == "undefined") continue;
 			gandsRef[gandsProps[c].extID].gandsPropertiesRef.push(gandsProps[c]);
 		}
 
-		for(c= 0, L=self.data.length; c<L; c++){
+		for (c = 0, L = self.data.length; c < L; c++) {
 			if (typeof self.GSUnits[self.data[c].GSID] == "undefined") {
 				self.GSUnits[self.data[c].GSID] = self.data[c].GSUnit
 			}
@@ -210,19 +211,19 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 		var configRow = self.dataReferences.get("ТСFM");
 
-		if (configRow){
+		if (configRow) {
 			// Заполнение недостающих полей в configRow
-			if (self.data.length){
+			if (self.data.length) {
 				var tmp = self.data[0];
-				for(var prop in tmp){
+				for (var prop in tmp) {
 					if (!Object.prototype.hasOwnProperty.call(tmp, prop)) continue;
-					if (  configRow[prop] === void 0  ) configRow[prop] = "";
+					if (configRow[prop] === void 0) configRow[prop] = "";
 				}
 			}
 
-			for(c=0; c<configRow.gandsPropertiesRef.length; c++){
-				if (configRow.gandsPropertiesRef[c].property == "номенклатура-группы"){
-					proto._matcherPatterns = proto._matcherPatterns.concat(eval("("+ configRow.gandsPropertiesRef[c].value +")"));
+			for (c = 0; c < configRow.gandsPropertiesRef.length; c++) {
+				if (configRow.gandsPropertiesRef[c].property == "номенклатура-группы") {
+					proto._matcherPatterns = proto._matcherPatterns.concat(eval("(" + configRow.gandsPropertiesRef[c].value + ")"));
 				}
 			}
 		}
@@ -235,7 +236,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	},
 
 
-	"_buildIndexData": function(){
+	"_buildIndexData": function() {
 
 		var match;
 
@@ -245,8 +246,8 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 			match = this._groupMatcher(this.data[c]);
 
-			for(var v=0; v<match.length; v++){
-				if (  typeof this._indexData[match[v]] != "object"  ){
+			for (var v = 0; v < match.length; v++) {
+				if (typeof this._indexData[match[v]] != "object") {
 					this._indexData[match[v]] = [];
 				}
 				this._indexData[match[v]].push(this.data[c]);
@@ -266,21 +267,21 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	/**
 	 * @ignore
 	 * */
-	"_groupMatcher": function(row){
+	"_groupMatcher": function(row) {
 
 		var tmp = [];
 
-		for(var c=0; c<this._matcherPatterns.length; c++){
+		for (var c = 0; c < this._matcherPatterns.length; c++) {
 			if (
 				this._matcherPatterns[c].GS
 				&& row.GSID.match(this._matcherPatterns[c].GS)
-			){
+			) {
 				tmp.push(this._matcherPatterns[c].gr);
 
 			} else if (
 				this._matcherPatterns[c].COP
 				&& row.GSCOP.match(this._matcherPatterns[c].COP)
-			){
+			) {
 				tmp.push(this._matcherPatterns[c].gr);
 			}
 		}
@@ -292,7 +293,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	/**
 	 * @ignore
 	 * */
-	"_indexHas": function(){
+	"_indexHas": function() {
 
 	},
 
@@ -300,12 +301,12 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	/**
 	 * @ignore
 	 * */
-	"_indexBuild": function(){
+	"_indexBuild": function() {
 
 	},
 
 
-	"_isDraft": function(row){
+	"_isDraft": function(row) {
 		return Boolean(row.GSName.match(/^[*]/gi) || row.GSKindName.match(/^[*]/gi));
 	},
 
@@ -315,19 +316,19 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	 * @param {Object | String} row
 	 * return {Object}
 	 * */
-	"getParent": function(row){
+	"getParent": function(row) {
 		if (
 			_utils.getType(row) == "object"
 			&& row.GSID
-		){
+		) {
 			return this.dataReferences.get(row.GSID.slice(0, -2))
 
-		} else if (  typeof row == "string"  ) {
+		} else if (typeof row == "string") {
 			row = this.dataReferences.get(row);
-			if (row && row.GSID.length >= 4){
+
+			if (row && row.GSID.length >= 4) {
 				return this.dataReferences.get(row.GSID.slice(0, -2))
 			}
-
 		}
 		return void 0;
 	},
@@ -340,13 +341,13 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	 * @param {Object=} options - параметры выборки
 	 * @return {undefined | Array}
 	 * */
-	"getProperty": function(row, props, options){
+	"getProperty": function(row, props, options) {
 		var ret = [];
 
-		if (  _utils.getType(row) == "object" && row.GSID  ){
-			row = this.dataReferences.get( row.GSID);
+		if (_utils.getType(row) == "object" && row.GSID) {
+			row = this.dataReferences.get(row.GSID);
 
-		} else if (  _utils.getType(row) == "string"  ) {
+		} else if (_utils.getType(row) == "string") {
 			row = this.dataReferences.get(row);
 
 		} else {
@@ -356,7 +357,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 		if (!row) return ret;
 
-		if (_utils.getType(props) == "array"){
+		if (_utils.getType(props) == "array") {
 
 		} else if (typeof props == "string") {
 			props = [props];
@@ -366,13 +367,13 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 		var props_ = [], row_ = row, c;
 
-		for(c=0; c<props.length; c++){
+		for (c = 0; c < props.length; c++) {
 			if (typeof props[c] != "string") continue;
 			props_[c] = props[c].toLowerCase();
 		}
 
 		do {
-			if (!props_.length){
+			if (!props_.length) {
 				ret = ret.concat(row_.gandsPropertiesRef);
 
 			} else {
@@ -383,24 +384,24 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 				}
 
 			}
-		} while(  row_ = this.getParent(row_)  );
+		} while (row_ = this.getParent(row_));
 
-		if (_utils.getType(options) != "object"){
+		if (_utils.getType(options) != "object") {
 			options = {};
 		}
 
-		if (options.onlyPriority){
+		if (options.onlyPriority) {
 			var priorProps = {};
-			for(c=0; c<ret.length; c++){
+			for (c = 0; c < ret.length; c++) {
 				if (
 					typeof priorProps[ret[c].property] != "object"
 					|| priorProps[ret[c].property].extID.length < ret[c].extID.length
-				){
+				) {
 					priorProps[ret[c].property] = ret[c];
 				}
 			}
 			ret = [];
-			for(var prop in priorProps){
+			for (var prop in priorProps) {
 				if (!priorProps.hasOwnProperty(prop)) continue;
 				ret.push(priorProps[prop]);
 			}
@@ -415,7 +416,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	 * @param {Array} arg.cop - Массив из RegExp для поиска среди КОПов
 	 * @param {Array} arg.type
 	 * */
-	"get" : function(arg){
+	"get": function(arg) {
 		if (typeof arg != "object") arg = Object.create(null);
 		var type = _utils.getType(arg.type) == "array" ? arg.type : [];
 		if (_utils.getType(arg.group) == "array") type = arg.group;
@@ -432,8 +433,8 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 			&& !cop.length
 			&& !flags.length
 			&& useDraft
-		){
-			if (  typeof this._indexData[type[0]] != "object"  ){
+		) {
+			if (typeof this._indexData[type[0]] != "object") {
 				return [];
 			}
 			return this._indexData[type[0]];
@@ -443,33 +444,33 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 
 		for (c = 0; c < this.data.length; c++) {
 
-			if (  tmp.indexOf(this.data[c]) > -1  ) continue;
+			if (tmp.indexOf(this.data[c]) > -1) continue;
 
-			if (  this._isDraft(this.data[c]) && !useDraft  ){
+			if (this._isDraft(this.data[c]) && !useDraft) {
 				continue;
 			}
 
-			if (  flags.length && !this.data[c].GSFlag.match(flagsRegEx)  ){
+			if (flags.length && !this.data[c].GSFlag.match(flagsRegEx)) {
 				continue;
 			}
 
 			match = this._groupMatcher(this.data[c]);
 
-			for(v=0; v<match.length; v++){
-				if (  type.indexOf(match[v]) > -1  ){
+			for (v = 0; v < match.length; v++) {
+				if (type.indexOf(match[v]) > -1) {
 					tmp.push(this.data[c]);
 					break;
 				}
 			}
 
-			for(v=0; v<cop.length; v++){
-				if (  cop[v] instanceof RegExp  ){
-					if (  this.data[c].GSCOP.match(cop[v])  ){
+			for (v = 0; v < cop.length; v++) {
+				if (cop[v] instanceof RegExp) {
+					if (this.data[c].GSCOP.match(cop[v])) {
 						tmp.push(this.data[c]);
 					}
 
-				} else if (  typeof cop[v].GSCOP == "string"  ) {
-					if (  cop[v] == this.data[c]  ){
+				} else if (typeof cop[v].GSCOP == "string") {
+					if (cop[v] == this.data[c]) {
 						tmp.push(this.data[c]);
 					}
 
@@ -481,7 +482,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	},
 
 
-	"getGSUnit": function(GSID){
+	"getGSUnit": function(GSID) {
 		if (typeof GSID == "undefined") return;
 
 		if (typeof this.GSUnits[GSID] == "undefined") return;
@@ -490,7 +491,7 @@ GandsDataModel.prototype = DefaultDataModel.prototype._objectsPrototyping(IEvent
 	},
 
 
-	"getJSON": function(){
+	"getJSON": function() {
 		var ret = Object.create(null);
 		ret.data = this.data;
 		ret.GSUnits = this.GSUnits;
