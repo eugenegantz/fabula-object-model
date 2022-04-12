e2f6cad3296f15845b1e980e74272408650f4989
eg <eugengantz@gmail.com>
2022-04-12 15:19:38 +0300

    upd: m-agents

--- fabula-object-model/data-models/AgentsDataModel.js
+++ fabula-object-model/data-models/AgentsDataModel.js
@@ -1,8 +1,9 @@
 "use strict";
 
-var voidFn      = function() {},
-	utils       = require("./../utils/utils.js"),
-	IFabModule  = require("./IFabModule.js");
+var voidFn      = function() {};
+var utils       = require("./../utils/utils.js");
+var ObjectA     = require("./ObjectA.js");
+var IFabModule  = require("./IFabModule.js");
 
 
 /**
@@ -65,28 +66,93 @@ AgentsDataModel.prototype = utils.createProtoChain(IFabModule.prototype, {
 			self = this;
 
 		if (!db) return;
+		
+		// agentsPropertiesRef
+		// agentsExtRef
 
 		var query = ""
 			+ " SELECT"
-			+   "   gsCodeNumber   AS AgentID"
-			+   " , importName     AS FIO"
-			+   " , gsName         AS NameShort"
-			+   " , importName     AS NameFull"
+			+   "   gsCodeNumber   AS [AgentID]"
+			+   " , importName     AS [FIO]"
+			+   " , gsName         AS [NameShort]"
+			+   " , importName     AS [NameFull]"
 			+   " , gsCop          AS [User]"
+			+   " , GSID           AS [GSID]"
 			+ " FROM Gands"
 			+ " WHERE"
-			+   " LEFT(GSID, 4) = 'SYСо'";
+			+   " LEFT(GSID, 4) = 'SYСо'"
+			
+			+ ";"
+			
+			+ " SELECT "
+			+   " [pid], [extID], [property], [value]"
+			+ " FROM Property "
+			+ " WHERE "
+			+   " LEFT(extid, 4) = 'SYСо'"
+			
+			+ ";"
+			
+			+ " SELECT"
+			+ "   [GEIDC], [GSExType], [GSExID], [GSExSort], [GSExExtID], [GSExName]"
+			+ " , [GSExNum], [GSExFlag], [GSExAttr1], [GSExAttr2], [Tick], [DateEdit]"
+			+ " FROM GandsExt"
+			+ " WHERE"
+			+   " LEFT(gsexid, 4) = 'SYСо'";
 
 		return db.query({
 			"dbcache": self.iFabModuleGetDBCache(arg.dbcache, { "m": "m-ag.load" }),
 			"query": query
 		}).then(function(res) {
-			self.data = res.recs;
+			var c;
+			var gandsRecs = res[0].recs;
+			var propsRecs = res[1].recs;
+			var gandsExtRecs = res[2].recs;
+
+			self.dataRefByGSID = ObjectA.create({});
+			self.dataRefByAgentId = ObjectA.create({});
+			
+			self.data = gandsRecs;
+			
+			for (c = 0; c < self.data.length; c++) {
+				var row = self.data[c];
+
+				self.dataRefByAgentId.set(row.AgentID + "", row);
+				self.dataRefByGSID.set(row.GSID, row);
+				
+				row.agentsPropertiesRef = [];
+				row.agentsExtRef = [];
+			}
+			
+			for (c = 0; c < propsRecs.length; c++) {
+				var propRow = propsRecs[c];
+				var extid = propRow.extID + "";
+				var agentRow = self.dataRefByGSID.get(extid);
+				
+				if (!agentRow)
+					continue;
+				
+				agentRow.agentsPropertiesRef.push(propRow);
+			}
+			
+			for (c = 0; c < gandsExtRecs.length; c++) {
+				var extRow = gandsExtRecs[c];
+				var gsexid = extRow.GSExID + "";
+				var agentRow = self.dataRefByGSID.get(gsexid);
+				
+				if (!agentRow)
+					continue;
+				
+				agentRow.agentsExtRef.push(extRow);
+			}
+
 			self.state = 1;
+			
 			callback(null, self, self.data);
 
 		}).catch(function(err) {
-			callback(err, self)
+			console.log(err);
+
+			callback(err, self);
 		});
 	},
 
