5a14bbdda96726f30ca0973ad4e0dbf4fa4a2e17
eg <eugengantz@gmail.com>
2025-08-31 04:33:59 +0300

    upd: InterfaceEvents
    add: removeListener(eventName, handler)

--- fabula-object-model/data-models/InterfaceEvents.js
+++ fabula-object-model/data-models/InterfaceEvents.js
@@ -22,41 +22,46 @@ EventsInterface.prototype = {
 
 	/**
 	 * Запускает указанное событие
+	 *
 	 * @param {String} eventName - название события
 	 * @param {CustomEvent | IEvent=} e - объект события
 	 * */
-	"trigger": function(eventName, e){
+	"trigger": function(eventName, e) {
 
-		if (typeof eventName != "string") return false;
+		if (typeof eventName != "string") {
+			return false;
+		}
 
 		eventName = eventName.toLowerCase();
 
 		if (
 			typeof this._iEventsListeners[eventName] == "object"
 			&& Array.isArray(this._iEventsListeners[eventName])
-		){
+		) {
 
 			if (
-				!(e instanceof IEvent)
+				   !(e instanceof IEvent)
 				&& !(e instanceof CustomEvent)
 				&& !(e instanceof Event)
-			){
+			) {
 				e = this._createEvent(eventName, e);
 			}
 
 			var events = this._iEventsListeners[eventName];
 
-			for(var c=0; c<events.length; c++){
+			for (var c = 0; c < events.length; c++) {
 
-				if (typeof events[c] != "function") continue;
+				if (typeof events[c] != "function") {
+					continue;
+				}
 
 				var event = events[c];
 
-				if (events[c].isOnce){
+				if (events[c].isOnce) {
 					this._removeListener(eventName, events[c]._unique_id);
 				}
 
-				event.apply(this,[this, e]);
+				event.apply(this, [this, e]);
 
 				event = null;
 
@@ -69,15 +74,14 @@ EventsInterface.prototype = {
 
 	/**
 	 * Устанавливает событие
+	 *
 	 * @param {String} eventName - название события
-	 * @param {Function} fx - функция выполняемая при запуске события
+	 * @param {Function} handler - функция выполняемая при запуске события
 	 * */
-	"on": function(eventName, fx){
-		// var c, self = this;
-
+	"on": function(eventName, handler) {
 		if (
-			typeof eventName != "string"
-			|| typeof fx != "function"
+			   typeof eventName != "string"
+			|| typeof handler != "function"
 		) {
 			return false;
 		}
@@ -87,46 +91,64 @@ EventsInterface.prototype = {
 		if (
 			typeof this._iEventsListeners[eventName] != "object"
 			|| !Array.isArray(this._iEventsListeners[eventName])
-		){
-
+		) {
 			this._iEventsListeners[eventName] = [];
-
 		}
 
-		fx._unique_id = Math.round(Math.random() * Math.pow(10,18));
+		handler._unique_id = Math.round(Math.random() * Math.pow(10,18));
 
-		if (  typeof fx.isOnce == "undefined"  ) fx.isOnce = false;
+		if (  typeof handler.isOnce == "undefined"  ) {
+			handler.isOnce = false;
+		}
 
-		this._iEventsListeners[eventName].push(fx);
+		this._iEventsListeners[eventName].push(handler);
 
 		return true;
-
 	},
 
 
 	/**
 	 * Удалить событие
+	 *
 	 * @param {String} eventName - название события
-	 * @param {*=} _unique_id - уникальный маркер события. Необходим если нужно удалить конкретный держатель события
+	 * @param {function|number} [handler] - обработчик события или уникальный маркер события. Необходим если нужно удалить конкретный держатель события
 	 * */
-	"_removeListener": function(eventName, _unique_id){
-		if (typeof _unique_id == "undefined"){
+	"removeListener": function(eventName, handler) {
+		this._removeListener(eventName, handler);
+	},
+
+
+	/**
+	 * Удалить событие
+	 *
+	 * @private
+	 *
+	 * @param {String} eventName - название события
+	 * @param {function|number} [handler] - обработчик события или уникальный маркер события. Необходим если нужно удалить конкретный держатель события
+	 * */
+	"_removeListener": function(eventName, handler) {
+		// _unique_id является legacy решением.
+		// Оставлен на всякий случай для обратной совместимости.
+
+		if (typeof handler == "undefined"){
 			this._iEventsListeners[eventName] = [];
+
 			return;
 		}
-		if (
-			typeof this._iEventsListeners[eventName] == "object"
-			&& Array.isArray(this._iEventsListeners[eventName])
-		){
-			var tmp = [];
-			var events = this._iEventsListeners[eventName];
-			for(var c=0; c<events.length; c++){
-				if (events[c]._unique_id == _unique_id){
-					continue;
-				}
-				tmp.push(events[c])
+
+		if (  !Array.isArray(this._iEventsListeners[eventName])  ) {
+			return;
+		}
+
+		var c = 0;
+		var events = this._iEventsListeners[eventName];
+
+		while (events[c]) {
+			if (  (events[c] == handler) || (events[c]._unique_id == handler)  ) {
+				events.splice(c, 1);
+			} else {
+				c++;
 			}
-			this._iEventsListeners[eventName] = tmp;
 		}
 	},
 
@@ -134,21 +156,23 @@ EventsInterface.prototype = {
 	/**
 	 * @private
 	 * */
-	"_createEvent": function(name, param){
+	"_createEvent": function(name, param) {
 		var event;
 
-		if (  !_utils.isBrowser()  ){
+		if (  !_utils.isBrowser()  ) {
 			event = new IEvent(name);
 
 		} else {
 			var EventConstructor = CustomEvent || Event;
 			event = new EventConstructor(name);
-
 		}
 
-		if (param && typeof param == "object"){
-			for(var prop in param){
-				if (  !Object.prototype.hasOwnProperty.call(param, prop)  ) continue;
+		if (param && typeof param == "object") {
+			for(var prop in param) {
+				if (  !Object.prototype.hasOwnProperty.call(param, prop)  ) {
+					continue;
+				}
+
 				event[prop] = param[prop];
 			}
 		}
@@ -172,34 +196,41 @@ EventsInterface.prototype = {
 
 	/**
 	 * Получить держатели событий
+	 *
 	 * @param {String} eventName
 	 * @return {Array}
 	 * */
-	"getListeners": function(eventName){
-		if (typeof eventName != "string"){
+	"getListeners": function(eventName) {
+		if (typeof eventName != "string") {
 			return [];
 		}
-		if (  typeof this._iEventsListeners[eventName] != "object"  ){
+
+		if (  typeof this._iEventsListeners[eventName] != "object"  ) {
 			return [];
 		}
+
 		return this._iEventsListeners[eventName];
 	},
 
 
 	/**
 	 * Установить событие, функция которой должна выполниться один раз
+	 *
 	 * @param {String} eventName
-	 * @param {Function} fx
+	 * @param {Function} handler
 	 * */
-	"once": function(eventName, fx){
+	"once": function(eventName, handler) {
 		if (
-			typeof fx != "function"
+			   typeof handler != "function"
 			|| typeof eventName != "string"
-		){
+		) {
 			return false;
 		}
-		fx.isOnce = true;
-		this.on(eventName, fx);
+
+		handler.isOnce = true;
+
+		this.on(eventName, handler);
+
 		return true;
 	}
 
